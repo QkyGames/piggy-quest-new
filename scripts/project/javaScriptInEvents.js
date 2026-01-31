c3d35a7fb3629884bc3283e6cf2254c012897720
QkyGames <qkygames@hotmail.com>
2026-01-31 10:22:17 -0300

    Add files via upload

---
+++ scripts/project/javaScriptInEvents.js
@@ -0,0 +1,21 @@
+
+
+const scriptsInEvents = {
+
+	async Esplash_Event1_Act7(runtime, localVars)
+	{
+
+	},
+
+	async Egame_Event1_Act1(runtime, localVars)
+	{
+
+	},
+
+	async Egame_Event421_Act4(runtime, localVars)
+	{
+
+	}
+};
+
+globalThis.C3.JavaScriptInEvents = scriptsInEvents;
